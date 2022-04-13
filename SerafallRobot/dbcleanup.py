e605f53ea6ae8ddbe7aeb90c214e0c13e5e9f66e
ItsSahilXd <103504075+ItsSahilXd@users.noreply.github.com>
2022-04-13 10:01:22 +0530

    Update dbcleanup.py

--- SerafallRobot/dbcleanup.py
+++ SerafallRobot/dbcleanup.py
@@ -1,9 +1,9 @@
 from time import sleep
 
-import AstrakoBot.modules.sql.global_bans_sql as gban_sql
-import AstrakoBot.modules.sql.users_sql as user_sql
-from AstrakoBot import DEV_USERS, OWNER_ID, dispatcher
-from AstrakoBot.modules.helper_funcs.chat_status import dev_plus
+import SerafallRobot.modules.sql.global_bans_sql as gban_sql
+import SerafallRobot.modules.sql.users_sql as user_sql
+from SerafallRobot import DEV_USERS, OWNER_ID, dispatcher
+from SerafallRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
